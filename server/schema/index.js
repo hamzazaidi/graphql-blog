a823dded5ada138164208d554960ddcdae46ce02
Hamza Zaidi <hamzazaidi@Hamzas-MBP.fios-router.home>
2021-02-18 14:28:22 -0500

    add mapping form post user posts and users

--- server/schema/index.js
+++ server/schema/index.js
@@ -1,7 +1,62 @@
-const { GraphQLObjectType, GraphQLList, GraphQLSchema, GraphQLID } = require("graphql");
+const { GraphQLObjectType, GraphQLList, GraphQLString, GraphQLSchema, GraphQLID } = require("graphql");
 const axios = require('axios');
-const { UserType } = require("./UserType");
-const { PostType } = require("./PostType");
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        address: { type: AddressType },     
+        posts: {
+            type: new GraphQLList(PostType),
+            async resolve(parent, args) {
+                const result = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${parent.id}`);
+                return result.data;
+            }
+        }   
+    })
+})
+
+const AddressType = new GraphQLObjectType({
+    name: 'Addresss',
+    fields: () => ({
+        street: { type: GraphQLString },
+        suite: { type: GraphQLString },
+        city: { type: GraphQLString },
+        zipcode: { type: GraphQLString },
+        geo: { type: GeoLocationType }
+    })
+})
+
+
+const GeoLocationType = new GraphQLObjectType({
+    name: 'GeoLocation',
+    fields: () => ({
+        lat: { type: GraphQLString },
+        lng: { type: GraphQLString },
+    })
+})
+
+
+
+const PostType = new GraphQLObjectType({
+    name: 'Post',
+    fields: () => ({
+        id: { type: GraphQLID },
+        title: { type: GraphQLString },
+        body: { type: GraphQLString },
+        user: {
+            type: UserType,
+            async resolve(parent, args) {
+                const result = await axios.get(`https://jsonplaceholder.typicode.com/users/${parent.userId}`);
+                return result.data;
+            }
+        }
+    })
+})
+
+
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
