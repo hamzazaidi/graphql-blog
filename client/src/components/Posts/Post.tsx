7cb73e26361da49f0e5cf332831802f70f5014b8
Hamza Zaidi <hamzazaidi@Hamzas-MBP.fios-router.home>
2021-02-20 21:46:40 -0500

    add selecting post on click

--- client/src/components/Posts/Post.tsx
+++ client/src/components/Posts/Post.tsx
@@ -1,40 +1,49 @@
 import { useQuery } from "@apollo/client";
+import { useState } from "react";
 import { Post } from "../../model/Post";
 import { GET_POSTS } from "../../queries";
 import Spinner from "../Spinner/Spinner";
 import './Posts.css';
 function Posts() {
     const { loading, error, data } = useQuery(GET_POSTS);
+    const [postId, setPostId] = useState('');
+
+    function selectPost(value: string) {
+        setPostId(value);
+    }
     function displayPosts() {
         if (loading) {
-          return <div className="card">
-            <div className="card-body">
-              <Spinner />
+            return <div className="card">
+                <div className="card-body">
+                    <Spinner />
+                </div>
             </div>
-          </div>
         } else {
-          return data.posts.map((u: Post) => {
-            return <div className="card" key={u.id}>
-              <div className="card-body d-flex flex-row shadow-lg p-3 bg-body rounded">
-                <div className="card-content">
-                  <h5 className="card-title">{u.title}</h5>  
-                  <h6 className="card-subtitle mb-2 text-muted">{ u.body }</h6>                      
-                </div>     
-                <div className="flex-fill">
-                    <img src={ u.user.avatar } alt="" className="shadow-sm mb-3 bg-body"/>
-                    <h6 className="card-subtitle mb-2 text-muted">{ u.user.name }</h6>                      
-                </div>       
-              </div>
-            </div>
-          })
+            return data.posts.map((u: Post) => {
+                return <div
+                        className={ u.id === postId ? 'card is-selected' : 'card' }
+                        key={u.id}
+                        onClick={() => selectPost(u.id)}                        
+                    >
+                    <div className="card-body d-flex flex-row shadow-lg p-3 bg-body rounded">
+                        <div className="card-content">
+                            <h5 className="card-title">{u.title}</h5>
+                            <h6 className="card-subtitle mb-2 text-muted">{u.body}</h6>
+                        </div>
+                        <div className="flex-fill">
+                            <img src={u.user.avatar} alt="" className="shadow-sm mb-3 bg-body" />
+                            <h6 className="card-subtitle mb-2 text-muted">{u.user.name}</h6>
+                        </div>
+                    </div>
+                </div>
+            })
         }
-      }
+    }
     return (
-      <div className="container posts">
-          { displayPosts() }
-      </div>
+        <div className="container posts">
+            { displayPosts()}
+        </div>
     );
-  }
-  
-  export default Posts;
-  
+}
+
+export default Posts;
